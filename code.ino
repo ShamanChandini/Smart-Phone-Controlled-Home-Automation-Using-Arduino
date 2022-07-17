c3f165dd8e8e13a68f312e94f1e88ee1ea0189e2
Shaman Chandini <89247662+ShamanChandini@users.noreply.github.com>
2022-07-17 12:41:37 +0530

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,15 @@
+#define BLYNK_TEMPLATE_ID "TMPL6Loplm52"
+#define BLYNK_DEVICE_NAME "bullayya"
+#define BLYNK_AUTH_TOKEN "0WaKyjiTEZbnTTgY9QowHZhTptNd6Rnk"
+#include <BlynkSimpleEsp8266.h>
+char a[] = BLYNK_AUTH_TOKEN;
+char b[] = "Lalitha_v";
+char c[] = "btsforever";
+void setup() 
+{  
+    // put your setup code here, to run once:Blynk.begin(a,b,c);
+    }
+void loop()
+ {  // put your main code here, to run repeatedly:Blynk.run();
+ 
+ }
